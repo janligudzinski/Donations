651812c03aa6fc2a1a5115900a5a7d7c6d786f89
oreganoli <3611916+oreganoli@users.noreply.github.com>
2025-01-24 16:19:52 +0100

    add role entity

---
+++ Donations/Entities/User/Role.cs
@@ -0,0 +1,5 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Donations.Entities.User;
+
+public class Role : IdentityRole<Guid>;
---
+++ Donations/Entities/User/Roles.cs
@@ -0,0 +1,7 @@
+namespace Donations.Entities.User;
+
+public static class Roles
+{
+    public const string Donor = "Donor";
+    public const string DonationCenter = "DonationCenter";
+}
