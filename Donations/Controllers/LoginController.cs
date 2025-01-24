d9e971901f83cfc1415e41aca810d640b9563b3a
oreganoli <3611916+oreganoli@users.noreply.github.com>
2025-01-24 17:45:18 +0100

    make it possible to log in

--- Donations/Controllers/LoginController.cs
+++ Donations/Controllers/LoginController.cs
@@ -1,10 +1,19 @@
+using Donations.Entities.User;
 using Donations.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Donations.Controllers;
 
 public class LoginController : Controller
 {
+    private readonly SignInManager<User> _signInManager;
+
+    public LoginController(SignInManager<User> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+
     public IActionResult Index(string? loginType)
     {
         LoginViewModel login = new LoginViewModel
@@ -12,5 +21,19 @@ public class LoginController : Controller
             IsDonationCenter = loginType == "DonationCenter"
         };
         return View(login);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Index(LoginFormModel loginFormModel)
+    {
+        var user = await _signInManager.PasswordSignInAsync(loginFormModel.Email, loginFormModel.Password, true, false);
+        if (!user.Succeeded)
+        {
+            return View("Index", new LoginViewModel
+            {
+                Failed = true
+            });
+        }
+        return RedirectToAction("Index", "Home");
     }
 }
--- Donations/Models/LoginViewModel.cs
+++ Donations/Models/LoginViewModel.cs
@@ -2,5 +2,6 @@ namespace Donations.Models;
 
 public class LoginViewModel
 {
+    public bool Failed { get; set; }
     public bool IsDonationCenter { get; set; } = false;
 }
