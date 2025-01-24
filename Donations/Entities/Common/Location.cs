3422cc015c0e4d7d66abb1db8aeb48fe842d39a7
oreganoli <3611916+oreganoli@users.noreply.github.com>
2025-01-24 22:25:37 +0100

    add distance method

--- Donations/Entities/Common/Location.cs
+++ Donations/Entities/Common/Location.cs
@@ -6,4 +6,23 @@ public class Location
     public string Name { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
+
+    public static double Distance(Location one, Location other)
+    {
+        // Haversine distance formula
+        double ToRadians(double angle)
+        {
+            return angle * (Math.PI / 180);
+        }
+        const double earthRadiusKilometers = 6371; // kilometers
+        var differenceLatitudes = ToRadians(one.Latitude - other.Latitude);
+        var differenceLongitudes = ToRadians(one.Longitude - other.Longitude);
+        
+        var a = Math.Sin(differenceLatitudes / 2) * Math.Sin(differenceLatitudes / 2) +
+                Math.Cos(ToRadians(one.Latitude)) * Math.Cos(ToRadians(other.Latitude)) *
+                Math.Sin(differenceLongitudes / 2) * Math.Sin(differenceLongitudes / 2);
+        var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+        var d = earthRadiusKilometers * c;
+        return d;
+    }
 }
