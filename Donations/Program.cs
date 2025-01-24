407c73332226c400dc027f2a9c15b0340fc300d8
oreganoli <3611916+oreganoli@users.noreply.github.com>
2025-01-24 16:26:27 +0100

    use db context

--- Donations/Program.cs
+++ Donations/Program.cs
@@ -1,7 +1,16 @@
+using Donations.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add database context
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
