32cf2e1dd78361365889f3faf893db191147e674
oreganoli <3611916+oreganoli@users.noreply.github.com>
2025-01-24 15:53:41 +0100

    define location type

---
+++ Donations/Entities/Common/Location.cs
@@ -0,0 +1,8 @@
+namespace Donations.Entities.Common;
+
+public class Location
+{
+    public string Name { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
