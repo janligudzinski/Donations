bebd50699e66b54e6ec1f582ddfc53dace41449e
oreganoli <3611916+oreganoli@users.noreply.github.com>
2025-01-27 16:07:16 +0100

    add blood supply entity

---
+++ Donations/Migrations/20250127150613_AddBloodSupplies.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Donations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBloodSupplies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BloodSupplies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BloodType = table.Column<int>(type: "int", nullable: false),
+                    MillilitersInStock = table.Column<int>(type: "int", nullable: false),
+                    DonationCenterId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BloodSupplies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BloodSupplies_DonationCenters_DonationCenterId",
+                        column: x => x.DonationCenterId,
+                        principalTable: "DonationCenters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BloodSupplies_DonationCenterId_BloodType",
+                table: "BloodSupplies",
+                columns: new[] { "DonationCenterId", "BloodType" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BloodSupplies");
+        }
+    }
+}
